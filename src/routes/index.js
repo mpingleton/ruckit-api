4300c6601d2cb5a3000a3fe2980d7a0029060b86
Michael Pingleton <mnpingleton@gmail.com>
2021-12-25 12:00:04 -0600

    Added call route.

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const authRoute = require('./auth');
 const userRoute = require('./user');
+const callRoute = require('./call');
 
 const router = express.Router();
 
 router.use('/auth', authRoute);
 router.use('/user', userRoute);
+router.use('/call', callRoute);
 
 module.exports = router;
