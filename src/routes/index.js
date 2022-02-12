ed9d5772249535ca41d9fc1acbb7221da93cb4c1
Michael Pingleton <mnpingleton@gmail.com>
2022-02-12 18:28:11 -0500

    Added message route.

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ const express = require('express');
 const authRoute = require('./auth');
 const userRoute = require('./user');
 const callRoute = require('./call');
+const messageRoute = require('./message');
 
 const router = express.Router();
 
 router.use('/auth', authRoute);
 router.use('/user', userRoute);
 router.use('/call', callRoute);
+router.use('/message', messageRoute);
 
 module.exports = router;
